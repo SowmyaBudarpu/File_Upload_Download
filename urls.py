92286f1058726a91f30bfab9ecb0e505bdf0a677
SowmyaBudarpu <130810724+SowmyaBudarpu@users.noreply.github.com>
2023-11-30 15:17:00 -0500

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,15 @@
+# file_manager/urls.py
+
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('file_manager_app.urls')),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
